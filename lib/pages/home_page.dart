9f0b533294fc60f3ed9c1f70ef457f98d91adb27
Agbai Iroha <agbai@myxrevents.com>
2021-10-21 01:32:31 -0400

    initial push

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import '../modals/recipe.dart';
+import '../widgets/recipe_card.dart';
+import './recipe_detail.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: Recipe.samples.length,
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              // 8
+              onTap: () {
+                // 9
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      // 10
+                      return RecipeDetail(recipe: Recipe.samples[index]);
+                    },
+                  ),
+                );
+              },
+              // 11
+              // child: buildRecipeCard(Recipe.samples[index]),
+              child: RecipeCard(recipe: Recipe.samples[index]),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
